02e72a1570d14f42c1bf0c9eb66267ad55398131
Evy Avila <384036@colum.edu>
2026-04-27 21:24:05 -0500

    start to fade progress

---
+++ Pact-Tactics-FullGame/Assets/Scripts/UI/FadeUI.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class FadeUI : AbstractMenuBase
+{
+    private VisualElement fade;
+
+    public override void SetProperties()
+    {
+        
+    }
+
+    public override void UnSetProperties()
+    {
+
+    }
+}
